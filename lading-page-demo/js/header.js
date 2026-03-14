f6b7340d96e0f3f4749b58329fdd5d3514e2a022
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-03-13 21:05:51 -0300

    Delete lading-page-demo/js/header.js

--- lading-page-demo/js/header.js
+++
@@ -1,6 +0,0 @@
-function abrirMenu() {
-  var elemento = document.getElementById("m-navlinks");
-
-  if (elemento.style.display === "none") elemento.style.display = "flex";
-  else elemento.style.display = "none";
-}
